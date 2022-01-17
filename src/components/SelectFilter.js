ea872b44134e6a0fa631d59e2ab789bd8d76d45d
vuoso-l <vuosolucas@gmail.com>
2022-01-17 19:19:40 -0300

    Modified fetch

--- src/components/SelectFilter.js
+++ src/components/SelectFilter.js
@@ -1,34 +1,44 @@
-import { useFetch } from "../hooks/useFetch";
+import React, { useState, useEffect } from "react";
 
-const SelectFilter = ({ url, handleChange }) => {
-  const { data, error, loading } = useFetch(url);
+const SelectFilter = ({ url, setIsLoaded, setError, handleChange }) => {
+  const [category, setCategory] = useState([]);
 
+  useEffect(() => {
+    const abortController = new AbortController();
+    fetch(url)
+      .then((response) => response.json())
+      .then(
+        (data) => {
+          setIsLoaded(true);
+          setCategory(data);
+        },
+        (error) => {
+          setIsLoaded(true);
+          setError(error);
+        }
+      );
+    return () => {
+      abortController.abort();
+    };
+  }, []);
+  
   let id = "category";
 
-  if (!data) return null;
-
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (loading) {
-    return <div>Loading...</div>;
-  } else {
-
-    return (
-      <>
-        <label htmlFor={id}>Elegí una categoría</label>
-        <select name={id} id={id} onChange={handleChange}>
-          <option value="">Elige una categoría</option>
-          <option value="all">Ver todas las categorías</option>
-          {data &&
-            data.map((el) => (
-              <option key={el} value={el}>
-                {el}
-              </option>
-            ))}
-        </select>
-      </>
-    );
-  }
+  return (
+    <>
+      <label htmlFor={id}>Elegí una categoría</label>
+      <select name={id} id={id} onChange={handleChange}>
+        <option value="">Elige una categoría</option>
+        <option value="all">Ver todas las categorías</option>
+        {category &&
+          category.map((el) => (
+            <option key={el} value={el}>
+              {el}
+            </option>
+          ))}
+      </select>
+    </>
+  );
 };
 
 export default SelectFilter;
