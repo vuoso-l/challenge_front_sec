29674ad438b983731409450fc01b05bb4673b660
vuoso-l <vuosolucas@gmail.com>
2022-01-18 18:47:29 -0300

    Added name and price to recommendation and return button

--- src/components/DetailsProduct.js
+++ src/components/DetailsProduct.js
@@ -10,11 +10,10 @@ const DetailsProduct = ({
 }) => {
   const handleModalContainerClick = (e) => e.stopPropagation();
 
-  /* setState(e => !e)
-   */
   return (
     <div className={`product ${isOpen && "is-open"}`} onClick={closeCard}>
       <div onClick={handleModalContainerClick}>
+        <button onClick={closeCard}>Volver</button>
         {recommendedImage.map((imgRec) => {
           return (
             <div className="product-container">
@@ -22,11 +21,13 @@ const DetailsProduct = ({
                 return (
                   prod.product_id === imgRec && (
                     <div>
+                      <h4>{prod.name}</h4>
                       <img
                         key={prod.product_id}
                         src={prod.image_url}
                         alt={prod.name}
                       ></img>
+                      <p>${prod.total_price}</p>
                       <button onClick={() => sumHandleCart(prod)}>
                         Agregar al carrito
                       </button>
