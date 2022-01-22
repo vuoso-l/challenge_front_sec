f7b39e97c0dfb83b2db915e5d7eb85ad25e38f52
vuoso-l <vuosolucas@gmail.com>
2022-01-22 20:22:21 -0300

    Modified Loader styled

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,41 @@
-import React from 'react';
-import "./Loader.css";
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const rotateLoadingRing = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const LoadingRingsDiv = styled.div`
+  position: relative;
+  padding: 20px 0;
+`;
+
+const LoadingRings = styled.div`
+  width: 64px;
+  height: 64px;
+  margin: 8px;
+  position: inherit;
+  top: calc(50% - 40px);
+  left: calc(50% - 40px);
+  border-top: 10px solid grey;
+  border-right: 8px solid #00bfff;
+  border-bottom: 8px solid #00bfff;
+  border-left: 8px solid #00bfff;
+  border-radius: 50%;
+  animation: ${rotateLoadingRing} 1.2s infinite;
+`;
 
 const Loader = () => {
-    return (
-        <div className="lds-ring">
-            <div></div>
-            <div></div>
-            <div></div>
-            <div></div>
-        </div>
-    )
-}
+  return (
+    <LoadingRingsDiv>
+      <LoadingRings />
+    </LoadingRingsDiv>
+  );
+};
 
 export default Loader;
