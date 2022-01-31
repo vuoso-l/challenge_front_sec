9df787e542e2d2a23ebc9f42f1cd3322304ce66e
vuoso-l <vuosolucas@gmail.com>
2022-01-31 08:51:41 -0300

    Added an image default when product without image

--- src/components/DetailsProduct.js
+++ src/components/DetailsProduct.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { ImgStyle, H4Style, AddButton } from "./BasicTagsStyle";
-import { DetailsProductStyle, DetailsProductContainer } from "./DetailsProductStyle";
+import {
+  DetailsProductStyle,
+  DetailsProductContainer,
+} from "./DetailsProductStyle";
 import { imgShopCart } from "../styleAux/fontAwesoneIcon";
 
 const DetailsProduct = ({
@@ -22,12 +25,20 @@ const DetailsProduct = ({
                 prod.product_id === imgRec && (
                   <div onClick={handleContainerClick} key={prod.product_id}>
                     <H4Style>{prod.name}</H4Style>
-                    <ImgStyle                    
-                      src={prod.image_url}
-                      alt={prod.name}
-                    ></ImgStyle>
+                    {prod.image_url ? (
+                      <ImgStyle src={prod.image_url} alt={prod.name}></ImgStyle>
+                    ) : (
+                      <ImgStyle
+                        src="//cdn.shopify.com/s/files/1/0254/2947/5433/t/12/assets/new_logo-black.svg?v=15514233714370248417?nocache=0.4027801373597353"
+                        alt="Logo"
+                      ></ImgStyle>
+                    )}
                     <H4Style>${prod.total_price}</H4Style>
-                    <AddButton add data-testid="btn-addCart" onClick={() => addHandleCart(prod)}>
+                    <AddButton
+                      add
+                      data-testid="btn-addCart"
+                      onClick={() => addHandleCart(prod)}
+                    >
                       {imgShopCart}
                     </AddButton>
                   </div>
