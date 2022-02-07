51eb3f51a4f0351711e9f116c9e8ba1198993afd
vuoso-l <vuosolucas@gmail.com>
2022-02-07 11:58:04 -0300

    Added messages from sweetAlert helpers

--- src/context/ElementCartContext.js
+++ src/context/ElementCartContext.js
@@ -1,6 +1,7 @@
 import React, { createContext, useEffect, useReducer } from "react";
 import { TYPES } from "../actions/elementCartActions";
 import { cartInitialState, cartReducers } from "../reducers/cartReducers";
+import SweetAlert from "../helpers/SweetAlert";
 
 const ElementCartContext = createContext();
 
@@ -14,15 +15,35 @@ const ElementCardProvider = ({ children }) => {
   }, [elementCart]);
 
   const handleClickToEmpty = () => {
-    dispatch({ type: TYPES.CLEAR_CART });
+    if (elementCart.length > 0) {
+      SweetAlert.messageDelete(`¿Seguro que quieres vaciar el carrito?`, () =>
+        dispatch({ type: TYPES.CLEAR_CART })
+      );
+    } else {
+      SweetAlert.messageError("El carrito está vacío");
+    }
   };
 
   const addHandleCart = (product) => {
     dispatch({ type: TYPES.ADD_TO_CART, payload: product });
+    SweetAlert.messageOk(
+      "Producto agregado al carrito!",
+      `Se agregó ${product.name}`
+    );
   };
 
   const deleteHandleCart = (product) => {
-    dispatch({ type: TYPES.DELETE_FROM_CART, payload: product });
+    const similarProduct = elementCart.find(
+      (item) => item.product.product_id === product.product_id
+    );
+    if (similarProduct) {
+      SweetAlert.messageDelete(
+        `¿Seguro que quieres eliminar la unidad "${product.name}" del carrito ?`,
+        () => dispatch({ type: TYPES.DELETE_FROM_CART, payload: product })
+      );
+    } else {
+      SweetAlert.messageError("No tienes este producto en tu carrito");
+    }
   };
 
   const data = {
